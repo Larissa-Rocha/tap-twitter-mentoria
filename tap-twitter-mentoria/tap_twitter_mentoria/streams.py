6dd427ea2c760f21c93640a047163afd2d4738a2
larissamendes <larissa.mendes@gupy.com.br>
2022-06-27 15:52:41 -0300

    add TweetsStream class

--- tap-twitter-mentoria/tap_twitter_mentoria/streams.py
+++ tap-twitter-mentoria/tap_twitter_mentoria/streams.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Optional, Union, List, Iterable
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
-from tap_twitter_mentoria.client import MySourceNameStream
+from tap_twitter_mentoria.client import TwitterStream
 
 # TODO: Delete this is if not using json files for schema definition
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
@@ -13,50 +13,23 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 #       - Copy-paste as many times as needed to create multiple stream types.
 
 
-class UsersStream(MySourceNameStream):
+class TweetsStream(TwitterStream):
     """Define custom stream."""
-    name = "users"
-    path = "/users"
+    name = "tweets"
+    path = "/tweets"
     primary_keys = ["id"]
     replication_key = None
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     # schema_filepath = SCHEMAS_DIR / "users.json"
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
         th.Property(
             "id",
             th.StringType,
-            description="The user's system ID"
+            description="The tweet ID"
         ),
         th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years"
-        ),
-        th.Property(
-            "email",
+            "text",
             th.StringType,
-            description="The user's email address"
+            description="The tweet text"
         ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format"
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
-
-
-class GroupsStream(MySourceNameStream):
-    """Define custom stream."""
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
     ).to_dict()
